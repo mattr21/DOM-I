71e46afeae97552b21c5d413cbeb400c2502eeb0
mattr21 <matt.rothstein21@gmail.com>
2019-01-28 15:49:16 -0800

    Finished main-content section

--- js/index.js
+++ js/index.js
@@ -38,7 +38,7 @@ const siteContent = {
 };
 
 // Example: Update the img src for the logo
-// this separates the code into two separate statements
+// this separates the code into two separate statements but you can combine them
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"]);
 
@@ -57,11 +57,28 @@ let h1Text = document.querySelector(".cta-text h1").innerHTML = "DOM <br> IS <br
 // Update button in cta section
 let buttonText = document.querySelector(".cta button").textContent = "Get Started";
 
+// Update h4s
+let h4s = document.querySelectorAll("h4");
+h4s[0].textContent = "FEATURES";
+h4s[1].textContent = "ABOUT";
+h4s[2].textContent = "SERVICES";
+h4s[3].textContent = "PRODUCT";
+h4s[4].textContent = "VISION";
+h4s[5].textContent = "CONTACT";
 
-
+// Update p
+let paragraphs = document.querySelectorAll("p");
+paragraphs[0].textContent = "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[1].textContent = "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[2].textContent = "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[3].textContent = "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[4].textContent = "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.";
+paragraphs[5].textContent = "";
+paragraphs[6].textContent = "";
+paragraphs[7].textContent = "";
+paragraphs[8].textContent = "";
 
 // Update img src for the circular code snippet picture in the cta section
-// this combines the statements which does the same thing as when they're separated
 let ctaCodeSnippet = document.getElementById("cta-img").src = siteContent["cta"]["img-src"];
 
 // Update the img src for the wide rectangle code snippet picture in the main-content section
